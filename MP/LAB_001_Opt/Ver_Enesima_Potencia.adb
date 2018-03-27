6d881cf6ae2f7d9eddf404cfdee77f28da06db9c
Unai Sainz de la Maza <unaihtc70@gmail.com>
2018-03-27 11:02:51 +0200

    Added optional lab code 0.1

--- MP/LAB_001_Opt/Ver_Enesima_Potencia.adb
+++ MP/LAB_001_Opt/Ver_Enesima_Potencia.adb
@@ -1,15 +1,14 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Ver_Potencia is
-   -- entrada: 1 natural, Num (EE)
+procedure Ver_Enesima_Potencia is
+   -- entrada: 2 natural
    -- salida: 4 naturales y 1 natural, Pot (SE)
    -- post: Los cuatro naturales corresponden a 4 de casos de prueba
-   --    potencias de 2, y Pot es 2 elevado a Num, 2**Num (Potencia de 2).
 
-   function Potencia (N:Natural) return Natural is
-   -- EJERCICIO 1- ESPECIFICA E IMPLEMENTA recursivamente el subprograma
-   --   Potencia que calcula la n-�sima potencia de 2.
+   function Potencia (M: Natural; N: Natural) return Natural is
+   -- EJERCICIO 1 (Opcional)- ESPECIFICA E IMPLEMENTA recursivamente el subprograma
+   --   Potencia que calcula la n-�sima potencia de M.
 
    begin
       -- Completar
@@ -18,32 +17,26 @@ procedure Ver_Potencia is
       end if;
 
       if N > 0 then
-        return 2 * Potencia(N-1);
+        return M * Potencia(M, N-1);
       end if;
 
-      NULL;
+      return 0;
    end Potencia;
-
-  Num: Natural;
 
 begin
       ---------- PRUEBAS EXPL�CITAS A PROBAR
    Put_Line ("--------------------------------");
-   Put("   CASO1: 2^0= "); put(2**0, 0); put(", y con tu programa es --> ");    Put (Potencia(0), 0); Put_Line(".");
+   Put("   CASO1: 2^0= "); put(2**0, 0); put(", y con tu programa es --> ");    Put (Potencia(2, 0), 0); Put_Line(".");
    New_Line;New_Line;
    Put_Line ("--------------------------------");
-   Put("   CASO2: 2^1= "); put(2**1, 0); put(", y con tu programa es --> ");    Put (Potencia(1), 0); Put_Line(".");
+   Put("   CASO2: 3^1= "); put(3**1, 0); put(", y con tu programa es --> ");    Put (Potencia(3, 1), 0); Put_Line(".");
    New_Line;New_Line;
    Put_Line ("--------------------------------");
-   Put("   CASO3: 2^5= "); put(2**5, 0); put(", y con tu programa es --> ");    Put (Potencia(5), 0); Put_Line(".");
+   Put("   CASO3: 5^5= "); put(5**5, 0); put(", y con tu programa es --> ");    Put (Potencia(5, 5), 0); Put_Line(".");
    New_Line;New_Line;
    Put_Line ("--------------------------------");
-   Put("   CASO4: 2^15= "); put(2**15, 0); put(", y con tu programa es --> ");    Put (Potencia(15), 0); put_line(".");
+   Put("   CASO4: 4^15= "); put(4**15, 0); put(", y con tu programa es --> ");    Put (Potencia(4, 15), 0); put_line(".");
    New_Line;New_Line;
    Put_Line ("--------------------------------");
    Put_Line ("--------------------------------");
-   Put("   CASO5: Elige el exponente: ");
-   Get (Num);
-   Put("       2^");put(Num,0);put("= ");put(2**Num,0);put(", y con tu programa es --> ");
-   Put(Potencia(Num), 0);
-end Ver_Potencia;
+end Ver_Enesima_Potencia;
