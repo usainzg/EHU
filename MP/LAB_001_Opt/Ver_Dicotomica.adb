c260ee6f3785d60a17a12fe7fd873ac75afe84a5
Unai Sainz de la Maza <unaihtc70@gmail.com>
2018-03-27 12:07:34 +0200

    Add busqueda dicotomica codigo

--- MP/LAB_001_Opt/Ver_Dicotomica.adb
+++ MP/LAB_001_Opt/Ver_Dicotomica.adb
@@ -9,20 +9,42 @@ procedure Ver_Dicotomica is
    I: Integer;
 
    function Dicotomica (Vector: T_Vector; N:Integer) return Integer is
-     begin 
-	-- COMPLETAR
-	Null;
+   mid : Integer := ((Vector'First + Vector'Last) / 2);
+   begin 
+	  -- COMPLETAR
+    if N > Vector'Size or Vector'Size = 0 then
+      return -1;
+    end if;
+
+    if Vector((mid)) = I then
+      return mid;
+    end if;
+
+    if Vector((mid)) > I then
+      return Dicotomica(Vector(Vector'First .. mid - 1), I);
+    end if;
+
+    if Vector((mid)) < I then
+      return Dicotomica(Vector(mid + 1 .. Vector'Last), I);
+    end if;
         
+    return -1;
    end Dicotomica;
 
-
+pos : Integer;
 begin
-   Put_Line ("Dame un vector ordenado de menor a mayor de 10 enteros, un entero Por linea:");
-   for J in 1 .. 10 loop
-      Get(Vector(J));
-   end loop;
-   Put_Line ("Dame un entero a buscar:");
-   Get (I);
-   Put_line ("La posición del entero a buscar es:");
-   Put (Dicotomica(Vector,I));
+    Put_Line ("Dame un vector ordenado de menor a mayor de 10 enteros, un entero Por linea:");
+    for J in 1 .. 10 loop
+        --Get(Vector(J));
+        Vector(J) := J;
+    end loop;
+    Put_Line ("Dame un entero a buscar:");
+    Get (I);
+    Put_line ("El resultado de la busqueda es:");
+    pos := Dicotomica(Vector,I);
+    if pos /= -1 then
+        Put_line("Encontrado y posicion: " & Integer'Image(pos));
+    else
+        Put_line("No encontrado.");
+    end if;
 end Ver_Dicotomica;
