c66958c5fe6e59b84bdb7463c97aa961acd76dbb
Unai Sainz de la Maza <unaihtc70@gmail.com>
2018-03-28 07:56:48 +0200

    Add EC projet using template

---
+++ EC/ProyectoEC/cmake-build-debug/CMakeFiles/Plantilla1718.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/fondos.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/fondos.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/graficos.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/graficos.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/main.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/main.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/rutserv.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/rutserv.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/sprites.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/sprites.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/teclado.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/teclado.c.o"
+  "/Users/unaisainz/EHU/EC/Plantilla1718/source/temporizadores.c" "/Users/unaisainz/EHU/EC/Plantilla1718/cmake-build-debug/CMakeFiles/Plantilla1718.dir/source/temporizadores.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
