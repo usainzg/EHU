5ccfc49a707b2b216fe8b89eb07ab5a6febefe7f
Unai Sainz de la Maza <unaihtc70@gmail.com>
2018-11-05 09:30:30 +0100

    add sbt

---
+++ EDA/Arboles2/src/clases/ToyTreeMap.java
@@ -0,0 +1,115 @@
+package clases;
+
+import java.util.Comparator;
+
+public class ToyTreeMap<Key, Value> {
+	private BinaryNode<Key, Value> root;
+	
+	private int size;
+	
+	private final Comparator<? super Key> keyComparator;
+	
+	public ToyTreeMap(Comparator<? super Key> keyComparator) {
+		this.root = null;
+		this.keyComparator = keyComparator;
+		this.size = 0;
+	}
+	
+	/* TIPO 0 */
+	public Value put(Key key, Value value) {
+		return put(key, value, this.root);
+	}
+	
+	private Value put(Key key, Value value, BinaryNode<Key, Value> node) {
+		if (key == null) throw new NullPointerException();
+		if (value == null) throw new IllegalArgumentException();
+		
+		// VACIA
+		if (node == null) {
+			BinaryNode<Key, Value> bNode = new BinaryNode<Key, Value>(key, value);
+			this.root = bNode;
+			size++;
+			return null;
+		}
+		
+		int cmp = keyComparator.compare(key, node.key);
+		if (cmp < 0) return put(key, value, node.left);
+		if (cmp > 0) return put(key, value, node.right);
+		
+		node.value = value;
+		return value;
+	}
+	
+	public Value get(Key key) {
+		if(key == null) throw new NullPointerException();
+		return get(key, this.root);
+	}
+	
+	private Value get(Key key, BinaryNode<Key, Value> node) {
+		if (node == null) return null;
+		int cmp = keyComparator.compare(key, node.key);
+		if (cmp < 0) return get(key, node.left);
+		if (cmp > 0) return get(key, node.right);
+		return node.value;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int clear() {
+		int sizeRet = this.size;
+		this.root = null;
+		size = 0;
+		return sizeRet;
+	}
+	
+	public void printLexicographically() {
+		
+	}
+	
+	private BinaryNode<Key, Value> getNode(Key key) {
+		if(key == null) throw new NullPointerException();
+		return getNode(key, this.root);
+	}
+	
+	private BinaryNode<Key, Value> getNode(Key key, BinaryNode<Key, Value> node) {
+		if (node == null) return null;
+		int cmp = keyComparator.compare(key, node.key);
+		if (cmp < 0) return getNode(key, node.left);
+		if (cmp > 0) return getNode(key, node.right);
+		return node;
+	}
+	
+	private boolean contains(Key key) {
+		return contains(key, this.root);
+	}
+	
+	private boolean contains(Key key, BinaryNode<Key, Value> node) {
+		if (key == null) return false;
+		int cmp = keyComparator.compare(key, node.key);
+		if (cmp < 0) return contains(key, node.left);
+		if (cmp > 0) return contains(key, node.right);
+		return true;
+	}
+	
+	
+	private static class BinaryNode<Key, Value> {
+		Key key;
+		Value value;
+        BinaryNode<Key, Value> left;
+        BinaryNode<Key, Value> right;
+
+//        BinaryNode(Key key, Value value, BinaryNode<Key, Value> left, BinaryNode<Key, Value> right) {
+//            this.key = key;
+//            this.value = value;
+//            this.left = left;
+//            this.right = right;
+//        }
+
+        BinaryNode(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+	}
+}
---
+++ EDA/Arboles2/src/main/Pruebas.java
@@ -0,0 +1,34 @@
+package main;
+
+import clases.KeyComparator;
+import clases.ToyTreeMap;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		ToyTreeMap<Integer, String> toyTreeMap = new ToyTreeMap<>(new KeyComparator());
+		
+		toyTreeMap.put(1, "Unai");
+		
+		String str = toyTreeMap.get(1);
+		
+		System.out.println(str);
+		
+		toyTreeMap.put(1, "Unai1");
+		toyTreeMap.put(2, "Unai2");
+		toyTreeMap.put(3, "Unai3");
+		toyTreeMap.put(4, "Unai4");
+		
+		System.out.println(toyTreeMap.size());
+		
+		if(toyTreeMap.get(10) == null) System.out.println("NULL");
+		
+		int size = toyTreeMap.clear();
+		
+		System.out.println("Tamaño antes: " + size);
+		
+		if(toyTreeMap.get(1) == null) System.out.println("VACIO");
+		
+	}
+
+}
